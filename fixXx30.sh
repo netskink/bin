ed19d5e0b6af3b7cdbda0bbd5980d0a0e148e98b
John F. Davis <davisjf@gmail.com>
2015-12-24 09:35:06 -0500

    another monitor

--- fixXx30.sh
+++ fixXx30.sh
@@ -3,10 +3,11 @@
 # LCD to right of laptop
 # xrandr --output LVDS-1 --auto --output VGA-1 --auto --right-of LVDS-1
 
-# LCD to left of laptop
-xrandr --output eDP1 --auto --output HDMI1 --auto --left-of eDP1
+# HDMI LCD to left of laptop
+#xrandr --output eDP1 --auto --output HDMI1 --auto --left-of eDP1
 
-
+# vga lcd to left of laptop
+#xrandr --output eDP1 --auto --output DP1 --auto --left-of eDP1
 # examples
 
 #xrandr --output VGA-0 --mode 1920x1080 --output VGA-1 --left-of  VGA-0 --mode 1920x1080
@@ -14,3 +15,9 @@ xrandr --output eDP1 --auto --output HDMI1 --auto --left-of eDP1
 #xrandr --output VGA-1 --auto --output VGA-0 --auto --right-of  VGA-1 
 #xrandr --output VBOX1 --auto --output VBOX0 --auto --right-of  VBOX1 
 
+# My LCD
+#xrandr --output eDP1 --auto --output DP1 --auto --left-of eDP1
+# samsung 
+xrandr --output LVDS-0 --auto --output VGA-0 --auto --left-of LVDS-0
+#killall fbpanel
+#fbpanel &
